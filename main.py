54dd869805876c912a458a0def3627c3570998c1
luddite478 <luddite478@gmail.com>
2018-12-04 17:09:29 +0300

    initial

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import subprocess as sp
+import numpy
+
+FFMPEG_BIN = "ffmpeg"
+command = [ FFMPEG_BIN,
+        '-i', 'fifo264',             # fifo is the named pipe
+        '-pix_fmt', 'bgr24',      # opencv requires bgr24 pixel format.
+        '-vcodec', 'rawvideo',
+        '-an','-sn',              # we want to disable audio processing (there is no audio)
+        '-f', 'image2pipe', '-']
+pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+
+while True:
+    raw_image = pipe.stdout.read(640*480*3)
+    image =  numpy.fromstring(raw_image, dtype='uint8')
+    image = image.reshape((480,640,3))
+    if image is not None:
+        cv2.imshow('Video', image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    pipe.stdout.flush()
+
+cv2.destroyAllWindows()
